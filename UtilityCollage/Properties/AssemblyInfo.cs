3da2d4b8d9561ea4e356da0d92fe765c2808e248
juliandrj <juliandrj@gmail.com>
2022-04-29 23:06:38 +0200

    Se agrega utilidad de impresión de datatables por consola

--- UtilityCollage/Properties/AssemblyInfo.cs
+++ UtilityCollage/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("UtilityCollage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utilitarios generales para aplicaciones varias")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("LaHuerta")]
 [assembly: AssemblyProduct("UtilityCollage")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ UtilityCollage/TablaConsolaHelper.cs
@@ -0,0 +1,69 @@
+﻿using ConsoleTables;
+using NLog;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace UtilityCollage
+{
+    public class TablaConsolaHelper
+    {
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+
+        public static void ImprimirDataSetConErrores(DataSet dataSet)
+        {
+            foreach (DataTable dt in dataSet.Tables)
+            {
+                _log.Info($"[{dt.Namespace}.{dt.TableName}]");
+                ImprimirDataTable(dt, true);
+            }
+        }
+
+        public static void ImprimirDataSet(DataSet dataSet)
+        {
+            foreach (DataTable dt in dataSet.Tables)
+            {
+                _log.Info($"[{dt.Namespace}.{dt.TableName}]");
+                ImprimirDataTable(dt, false);
+            }
+        }
+
+        public static void ImprimirDataTable(DataTable dataTable)
+        {
+            ImprimirDataTable(dataTable, false);
+        }
+
+        public static void ImprimirDataTable(DataTable dataTable, bool incluirRowError)
+        {
+            List<string> columnas = new List<string>();
+            if (incluirRowError)
+            {
+                columnas.Add("ERROR");
+            }
+            foreach (DataColumn col in dataTable.Columns)
+            {
+                columnas.Add(col.ColumnName);
+            }
+            ConsoleTable ct = new ConsoleTable(columnas.ToArray<string>());
+            int i = 1;
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                if (i >= 10)
+                {
+                    break;
+                }
+                List<object> valores = new List<object>();
+                if (incluirRowError)
+                {
+                    valores.Add(dr.RowError);
+                }
+                valores.AddRange(dr.ItemArray);
+                ct.AddRow(valores.ToArray());
+                i++;
+            }
+            _log.Info($"\n{ct.ToMinimalString()}");
+            _log.Info($"Se mostraron {i - 1} filas de {dataTable.Rows.Count}");
+            _log.Info("########################################################");
+        }
+    }
+}
