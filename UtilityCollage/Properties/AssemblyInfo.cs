58a56275af6f3a0d4ab068466253c1048d4e64ee
Julian Rojas <julian.rojas@inetum.com>
2023-03-27 12:13:37 +0200

    Ajustes en la generación del paquete nuget

--- UtilityCollage/Properties/AssemblyInfo.cs
+++ UtilityCollage/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("UtilityCollage")]
 [assembly: AssemblyDescription("Utilitarios generales para aplicaciones varias")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LaHuerta")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UtilityCollage")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
