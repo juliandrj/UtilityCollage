72af2be2b01709731c9ca70176f5b49f96b136b9
Julian Rojas <julian.rojas@inetum.com>
2024-05-20 13:02:01 +0200

    Ajuste en número de filas a mostrar.

--- UtilityCollage/TablaConsolaHelper.cs
+++ UtilityCollage/TablaConsolaHelper.cs
@@ -9,31 +9,47 @@ namespace UtilityCollage
     public class TablaConsolaHelper
     {
         private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+        private static readonly int NumeroMaximoDeFilasAMostrarPorDefecto = 50;
 
-        public static void ImprimirDataSetConErrores(DataSet dataSet)
+        public static void ImprimirDataSetConErrores(DataSet dataSet, int numeroFilasMaximasAMostrar)
         {
             foreach (DataTable dt in dataSet.Tables)
             {
                 _log.Info($"[{dt.Namespace}.{dt.TableName}]");
-                ImprimirDataTable(dt, true);
+                ImprimirDataTable(dt, true, numeroFilasMaximasAMostrar);
+            }
+        }
+
+        public static void ImprimirDataSetConErrores(DataSet dataSet)
+        {
+            ImprimirDataSetConErrores(dataSet, NumeroMaximoDeFilasAMostrarPorDefecto);
+        }
+
+        public static void ImprimirDataSet(DataSet dataSet, int numeroFilasMaximasAMostrar)
+        {
+            foreach (DataTable dt in dataSet.Tables)
+            {
+                _log.Info($"[{dt.Namespace}.{dt.TableName}]");
+                ImprimirDataTable(dt, false, numeroFilasMaximasAMostrar);
             }
         }
 
         public static void ImprimirDataSet(DataSet dataSet)
         {
-            foreach (DataTable dt in dataSet.Tables)
-            {
-                _log.Info($"[{dt.Namespace}.{dt.TableName}]");
-                ImprimirDataTable(dt, false);
-            }
+            ImprimirDataSet(dataSet, NumeroMaximoDeFilasAMostrarPorDefecto);
         }
 
-        public static void ImprimirDataTable(DataTable dataTable)
+        public static void ImprimirDataTable(DataTable dataTable, int numeroFilasMaximasAMostrar)
         {
             ImprimirDataTable(dataTable, false);
         }
 
-        public static void ImprimirDataTable(DataTable dataTable, bool incluirRowError)
+        public static void ImprimirDataTable(DataTable dataTable)
+        {
+            ImprimirDataTable(dataTable, false, NumeroMaximoDeFilasAMostrarPorDefecto);
+        }
+
+        public static void ImprimirDataTable(DataTable dataTable, bool incluirRowError, int numeroFilasMaximasAMostrar)
         {
             List<string> columnas = new List<string>();
             if (incluirRowError)
@@ -48,7 +64,7 @@ namespace UtilityCollage
             int i = 1;
             foreach (DataRow dr in dataTable.Rows)
             {
-                if (i >= 10)
+                if (i >= numeroFilasMaximasAMostrar)
                 {
                     break;
                 }
@@ -64,6 +80,11 @@ namespace UtilityCollage
             _log.Info($"\n{ct.ToMinimalString()}");
             _log.Info($"Se mostraron {i - 1} filas de {dataTable.Rows.Count}");
             _log.Info("########################################################");
+        }
+
+        public static void ImprimirDataTable(DataTable dataTable, bool incluirRowError)
+        {
+            ImprimirDataTable(dataTable, incluirRowError, NumeroMaximoDeFilasAMostrarPorDefecto);
         }
     }
 }
